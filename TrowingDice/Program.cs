47a8c542b54b7ec1128169b998ce48246da60d94
a21jeaha <hisui86@gmail.com>
2025-01-23 18:32:22 +0100

    initial commit with a dice class created

--- TrowingDice/Program.cs
+++ TrowingDice/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
-using System.IO; 
+using System.IO;
+using TrowingDice;
 
-
+/**	
+ *  This is remade version of my first ever programing assignment.
+**/
 namespace ThrowingDice
 {
 	internal class TrowingDice
 	{
 		static void Main(string[] args)
 		{
+			Dice dice = new Dice();
+
+			for (int i = 0; i < 20; i++)
+			{
+				Console.WriteLine(dice.ThrowDice());
+
+			}
+
 			Console.WriteLine("Hello, World!");
 		}
 	}
