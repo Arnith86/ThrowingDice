0693636022698ee84a82292533af3528985470d7
a21jeaha <hisui86@gmail.com>
2025-01-23 17:51:48 +0100

    Add project files.

---
+++ TrowingDice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO; 
+
+
+namespace ThrowingDice
+{
+	internal class TrowingDice
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
+
+
