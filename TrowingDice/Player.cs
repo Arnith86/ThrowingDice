70d0905a3182a1e4f37b930a83d958394c7392e7
a21jeaha <hisui86@gmail.com>
2025-01-26 15:12:40 +0100

    added a new class, Player. It will keep track of player deposit and bet.

---
+++ TrowingDice/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrowingDice
+{
+	/** 
+	 * Player class: keeps track of current balance and bet.
+	 **/
+	class Player
+	{
+		private int deposit;
+		private int bet; 
+
+		public Player()
+		{
+			deposit = 0;
+			bet = 0;
+		}
+
+		public void setBet(int bet)
+		{
+			this.bet = bet;
+		}
+
+		public int getBet() 
+		{
+			return this.bet;
+		}
+
+		public void setDeposit(int deposit) 
+		{
+			this.deposit = deposit;
+		}
+
+		public int getDeposit() 
+		{
+			return this.deposit;
+		}
+	}
+}
